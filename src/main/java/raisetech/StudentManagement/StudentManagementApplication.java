d59ec34b3fafa360123258d60349d47cd1d5c39d
310Go <310Gosho@gmail.com>
2025-03-05 13:50:59 +0900

    スタンダード8回（講座）終了

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,21 +3,41 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Saito Shogo";
+	private String age = "25";
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/gimon")
-	public String astugiri() {
-		return "Why Japanese people!?";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name +" "+ age;
 	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name,String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@GetMapping("/studentName")
+	public  void updateStudentName(String name) {
+		this.name = name;
+	}
+
+	//GETとPOST
+	//Getは取得
+	//POSTは渡す
+
 }
 
 //授業が終わったらプッシュ
