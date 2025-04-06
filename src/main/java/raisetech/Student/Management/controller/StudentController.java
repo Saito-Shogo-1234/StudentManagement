73b68498595f013341d1a0bc8079a1a9967e0506
310Go <310Gosho@gmail.com>
2025-04-06 11:19:10 +0900

    スタンダード23回（講座）途中

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -3,10 +3,14 @@ package raisetech.Student.Management.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.Student.Management.controller.converter.StudentConverter;
 import raisetech.Student.Management.data.StudentsCourses;
 import raisetech.Student.Management.data.Student;
+import raisetech.Student.Management.domain.StudentDetail;
 import raisetech.Student.Management.service.StudentService;
 import org.springframework.ui.Model;
 
@@ -15,7 +19,7 @@ import org.springframework.ui.Model;
 public class StudentController {
 
   private StudentService service;
-  private  StudentConverter converter;
+  private StudentConverter converter;
 
   @Autowired
   public StudentController(StudentService service, StudentConverter converter) {
@@ -35,5 +39,19 @@ public class StudentController {
   @GetMapping("/studentsCourseList")
   public List<StudentsCourses> getStudentsCourseList() {
     return service.searchStudentsCourseList();
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model){
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    if(result.hasErrors()){
+      return "registerStudent";
+    }
+    return "redirect:/studentList";
   }
 }
